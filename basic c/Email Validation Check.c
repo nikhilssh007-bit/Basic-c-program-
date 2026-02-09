2c874eb6e6812b46ef612290dddd03017ae507d0
Nikhil 007 <nikhilssh007@gmail.com>
2026-02-09 16:27:04 +0530

    Create Email Validation Check.c

---
+++ basic c/Email Validation Check.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int length;
+    scanf("%d", &length);
+
+    if (length > 5)
+        printf("Valid Email");
+    else
+        printf("Invalid Email");
+
+    return 0;
+}
