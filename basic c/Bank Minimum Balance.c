a8ce119ee57ffd148acd85d1343033ec798d06ae
Nikhil 007 <nikhilssh007@gmail.com>
2026-02-09 16:18:21 +0530

    Create Bank Minimum Balance.c

---
+++ basic c/Bank Minimum Balance.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int balance;
+    scanf("%d",&balance);
+        if(balance <= 1000)
+        printf("low balance");
+        else
+        printf("sufficient balance");
+    return 0;
+}
