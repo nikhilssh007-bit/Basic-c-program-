425c8656bceecc3fc49d4b9b12080522a512c460
Nikhil 007 <nikhilssh007@gmail.com>
2026-02-09 16:27:35 +0530

    Create Library Book Fine.c

---
+++ basic c/Library Book Fine.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int days;
+    scanf("%d", &days);
+
+    if (days > 7)
+        printf("Fine Applied");
+    else
+        printf("No Fine");
+
+    return 0;
+}
