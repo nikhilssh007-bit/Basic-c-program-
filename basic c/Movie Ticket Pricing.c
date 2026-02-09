ad01c000d3a0a639570380dae5c32fdb83e5d0a0
Nikhil 007 <nikhilssh007@gmail.com>
2026-02-09 16:16:06 +0530

    Create Movie Ticket Pricing.c

---
+++ basic c/Movie Ticket Pricing.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int age;
+    scanf("%d",&age);
+    if(age < 12)
+    printf("ticket discount");
+    else
+    printf("regular ticket");    
+    return 0;
+}
