7c29c0d78b0bf9c312cde57be1a0c2bbf05788dc
Nikhil 007 <nikhilssh007@gmail.com>
2026-02-09 16:17:01 +0530

    Create Exam Attendance Eligibility.c

---
+++ basic c/Exam Attendance Eligibility.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int attendance;
+    scanf("%d",&attendance);
+    if(attendance >= 75)
+    printf("eligible for exam");
+    else
+    printf("not eligible for exam");
+    return 0;
+}
