0ea7e79cff2c16c0a59b2d4c41d5418142049e9d
Nikhil 007 <nikhilssh007@gmail.com>
2026-02-13 21:19:32 +0530

    Create Loan EMI Default Streak Analyzer.C

---
+++ basic c/NESTED LOOP/Loan EMI Default Streak Analyzer.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n,i=0,val,streak=0,maxStreak=0;
+    scanf("%d",&n);
+    while(i<n){
+        scanf("%d",&val);
+        if(val==0){
+            streak++;
+            if(streak>maxStreak)
+                maxStreak=streak;
+        } else streak=0;
+        i++;
+    }
+    printf("Longest Default Streak: %d",maxStreak);
+    return 0;
+}
