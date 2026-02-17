8c7bd31db018f255ed7c8605f44c294c9ff19d7f
Nikhil 007 <nikhilssh007@gmail.com>
2026-02-17 21:19:50 +0530

    Create  Left Rotate by K.C

---
+++ basic c/1D ARRAY/Left Rotate by K.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n,k;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    scanf("%d",&k);
+
+    for(int i=0;i<k;i++){
+        int temp=a[0];
+        for(int j=0;j<n-1;j++)
+            a[j]=a[j+1];
+        a[n-1]=temp;
+    }
+
+    for(int i=0;i<n;i++) printf("%d ",a[i]);
+    return 0;
+}
