5cb6e0488128e65b463e898ac92a7d0abd855dc8
Nikhil 007 <nikhilssh007@gmail.com>
2026-02-18 19:23:42 +0530

    Create Cheapest Valid Product.C

---
+++ basic c/1D ARRAY/Cheapest Valid Product.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    int min=-1;
+
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+        if(a[i] > 0){
+            if(min==-1 || a[i] < min)
+                min=a[i];
+        }
+    }
+
+    if(min==-1) printf("No positive");
+    else printf("%d",min);
+
+    return 0;
+}
