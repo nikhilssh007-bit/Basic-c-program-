e0ec941c21fc43777b8a620ff9245cca61a5297c
Nikhil 007 <nikhilssh007@gmail.com>
2026-02-18 19:21:03 +0530

    Create Leader Elements.C

---
+++ basic c/1D ARRAY/Leader Elements.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+
+    int max=a[n-1];
+    printf("%d ",max);
+
+    for(int i=n-2;i>=0;i--){
+        if(a[i]>max){
+            max=a[i];
+            printf("%d ",max);
+        }
+    }
+    return 0;
+}
