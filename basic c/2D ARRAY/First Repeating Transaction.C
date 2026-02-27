d1db4c61028c9edd5618e7b6825138a8f3c02fcd
Nikhil 007 <nikhilssh007@gmail.com>
2026-02-27 20:00:18 +0530

    Create First Repeating Transaction.C

---
+++ basic c/2D ARRAY/First Repeating Transaction.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+    int r,c,a[50][50];
+
+    scanf("%d%d",&r,&c);
+
+    for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++)
+            scanf("%d",&a[i][j]);
+
+    for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++)
+            for(int k=i;k<r;k++)
+                for(int l=0;l<c;l++)
+                    if((i!=k || j!=l) && a[i][j]==a[k][l]){
+                        printf("%d",a[i][j]);
+                        return 0;
+                    }
+
+    return 0;
+}
