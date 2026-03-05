78ea58b9b8537508eabcc3a3fe2a15b69b8ab149
Nikhil 007 <nikhilssh007@gmail.com>
2026-03-05 11:58:21 +0530

    Add program to count rows with only prime numbers
    This program counts the number of rows in a 2D array that contain only prime numbers. It reads the dimensions and elements of the array, checks each row for prime numbers, and outputs the count.

---
+++ basic c/2D ARRAY/Rows Containing Only Prime Numbers.C
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main()
+{
+    int r,c,i,j,k,a[100][100],count=0;
+
+    scanf("%d %d",&r,&c);
+
+    for(i=0;i<r;i++)
+        for(j=0;j<c;j++)
+            scanf("%d",&a[i][j]);
+
+    for(i=0;i<r;i++)
+    {
+        int allPrime=1;
+
+        for(j=0;j<c;j++)
+        {
+            int num=a[i][j],prime=1;
+
+            if(num<2) prime=0;
+
+            for(k=2;k<=num/2;k++)
+                if(num%k==0)
+                    prime=0;
+
+            if(!prime)
+                allPrime=0;
+        }
+
+        if(allPrime)
+            count++;
+    }
+
+    printf("%d",count);
+
+    return 0;
+}
