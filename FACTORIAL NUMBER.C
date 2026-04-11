21ff9ddd2d5f90a94e7c4afc35fab699c939ab38
Nikhil 007 <nikhilssh007@gmail.com>
2026-04-11 21:46:44 +0530

    Create FACTORIAL NUMBER.C

---
+++ FACTORIAL NUMBER.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, num = 5, fact = 1;
+
+    for(i = 1; i <= num; i++)
+    {
+        fact = fact * i;
+    }
+
+    printf("Factorial = %d\n", fact);
+
+    return 0;
+}
