93c4412e99987e38bd6e9a66ea58227001b5108d
Nikhil 007 <nikhilssh007@gmail.com>
2026-04-12 07:33:59 +0530

    Create PRIME NUMBER CHECK.C

---
+++ PRIME NUMBER CHECK.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int n, i, flag = 1;
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    for(i = 2; i <= n/2; i++) {
+        if(n % i == 0) {
+            flag = 0;
+            break;
+        }
+    }
+
+    if(flag == 1)
+        printf("Prime");
+    else
+        printf("Not Prime");
+
+    return 0;
+}
