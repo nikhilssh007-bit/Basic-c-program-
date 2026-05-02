47d086f9c0774860b44347c86f6cae98cadf8798
Nikhil 007 <nikhilssh007@gmail.com>
2026-05-02 20:40:30 +0530

    Create FILE READ.C

---
+++ FILE READ.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    char ch;
+
+    fp = fopen("test.txt", "r");
+
+    while((ch = fgetc(fp)) != EOF)
+        printf("%c", ch);
+
+    fclose(fp);
+    return 0;
+}
